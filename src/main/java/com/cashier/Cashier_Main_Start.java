87cc9923546dc8a794bbbf2f12c50ea48747866f
liurenwei <13246526052@163.com>
2019-10-10 11:54:44 +0800

    项目结构初始化

---
+++ src/main/java/com/cashier/Cashier_Main_Start.java
@@ -0,0 +1,13 @@
+
+package com.cashier;
+/**
+ * 收银系统主启动类: Cashier_Main_Start
+ * Company: www.91changqi.com
+ * @author liurenwei
+ * @Date 2019年10月10日
+ * @version 1.0
+ */
+public class Cashier_Main_Start {
+	
+	
+}
---
+++ src/main/java/com/cashier/Readme.java
@@ -0,0 +1,21 @@
+package com.cashier;
+
+/**
+ * <p>Readme: 项目说明 </p>  
+ * <p>Company: www.91changqi.com</p>  
+ * @author liurenwei
+ * @Date 2019年10月10日
+ * @version 1.0  
+ */
+public class Readme {
+	/**
+	 * 项目初始结构：
+	 * 	  common：存放一些通用的类
+	 *    controller： 存放controller类
+	 *    mapper： 存放mybatis的mapper及mapper接口
+	 *    pojo： 存放项目的实体类
+	 *    service： 存放service接口，imp：存放service接口的实现类
+	 *    utils： 存放项目的工具类
+	 * 
+	 */
+}
